6b1aae9ab365657571365507d8f448ca835ed370
jvan <jvan@jvanlabs.net>
2013-08-15 11:04:56 -0700

    Replaced HTML tag with img_tag helper.
    Fixes prefix issue when deploying.

--- helpers/vroom_helpers.rb
+++ helpers/vroom_helpers.rb
@@ -3,7 +3,7 @@ module VroomHelpers
    def vroom_example_image(modname, function, filename)
       relpath = "examples/#{modname}/#{function}/#{filename}"
       if File.exists?("./source/assets/img/#{relpath}")
-         "<img src='/assets/img/#{relpath}' width='400' height='225'/>"
+         image_tag relpath, :width=>400, :height=>225
       else
          '<img src="holder.js/400x225/social"/>'
       end
